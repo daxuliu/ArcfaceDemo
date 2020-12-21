6e21957f36636c811df63e79933e279b04621a40
Kitsch <5256280+wsldx@user.noreply.gitee.com>
2020-12-21 16:43:05 +0800

    Initial commit

---
+++ app/src/main/java/com/arcsoft/arcfacedemo/common/Constants.java
@@ -0,0 +1,6 @@
+package com.arcsoft.arcfacedemo.common;
+
+public class Constants {
+    public static final String APP_ID = "CEktGViWvCSMpdGjaUtEyX3NFa9egCCCVGxpWFCRRVgb";
+    public static final String SDK_KEY = "E243sYK2E5qR7qRbZ6prkrdGtqr8zsyueyHoipFNUFfh";
+}
